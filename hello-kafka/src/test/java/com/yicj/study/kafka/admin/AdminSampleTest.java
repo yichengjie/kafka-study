f07a136f67841f90fadfca08aba44941c662085f
易成杰 <626659321@qq.com>
2022-12-04 16:17:05 +0800

    save

--- hello-kafka/src/test/java/com/yicj/study/kafka/admin/AdminSampleTest.java
+++ hello-kafka/src/test/java/com/yicj/study/kafka/admin/AdminSampleTest.java
@@ -4,10 +4,10 @@ package com.yicj.study.kafka.admin;
 import com.yicj.study.kafka.constants.CommonConstants;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.admin.*;
+import org.apache.kafka.common.KafkaFuture;
 import org.junit.Test;
-import java.util.Arrays;
-import java.util.Properties;
-import java.util.Set;
+
+import java.util.*;
 import java.util.concurrent.ExecutionException;
 
 @Slf4j
@@ -39,6 +39,39 @@ public class AdminSampleTest {
         ListTopicsResult result = adminClient.listTopics(options);
         Set<String> strings = result.names().get();
         strings.stream().forEach(name -> log.info("name : {}", name));
+    }
+
+    @Test
+    public void describeTopic() throws ExecutionException, InterruptedException {
+        AdminClient adminClient = initAdminClient();
+        DescribeTopicsResult result = adminClient.describeTopics(Arrays.asList(TOPIC_NAME));
+        Map<String, TopicDescription> descriptionMap = result.all().get();
+        descriptionMap.forEach((name, describe) -> {
+            log.info("======>  describe : {}", describe);
+        });
+        //(name=hello-topic, internal=false,
+        //  partitions=
+        //      (partition=0, leader=192.168.99.51:9092 (id: 0 rack: null),
+        //          replicas=192.168.99.51:9092 (id: 0 rack: null),
+        //          isr=192.168.99.51:9092 (id: 0 rack: null)),
+        //      (partition=1, leader=192.168.99.51:9092 (id: 0 rack: null),
+        //           replicas=192.168.99.51:9092 (id: 0 rack: null),
+        //           isr=192.168.99.51:9092 (id: 0 rack: null)))
+    }
+
+    /**
+     * 增加partition 的数据量
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
+    @Test
+    public void incrementPartitions() throws ExecutionException, InterruptedException {
+        AdminClient adminClient = initAdminClient();
+        Map<String, NewPartitions> newPartitionsMap = new HashMap<>() ;
+        NewPartitions newPartitions = NewPartitions.increaseTo(2) ;
+        newPartitionsMap.put(TOPIC_NAME, newPartitions) ;
+        CreatePartitionsResult result = adminClient.createPartitions(newPartitionsMap);
+        result.all().get();
     }
 
     private AdminClient initAdminClient (){
---
+++ hello-kafka/src/test/java/com/yicj/study/kafka/admin/ProducerSampleTest.java
@@ -0,0 +1,56 @@
+package com.yicj.study.kafka.admin;
+
+import com.yicj.study.kafka.constants.CommonConstants;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.*;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.Future;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@Slf4j
+public class ProducerSampleTest {
+
+    private final static String TOPIC_NAME = "hello-topic" ;
+
+    @Test
+    public void producer(){
+        Producer<String, String> producer = this.initProducer();
+        // 消息对象
+        IntStream.range(0, 9)
+                .forEach(index -> {
+                    String key = String.format("key-%s", (index + 1));
+                    String value = String.format("hello world [%s]", (index + 1));
+                    ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC_NAME, key, value);
+                    Future<RecordMetadata> future = producer.send(record);
+                    try {
+                        RecordMetadata metadata = future.get();
+                        log.info("index : {}, partition: {}, offset: {}", (index +1) , metadata.partition(), metadata.offset());
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
+                });
+        // 所有的通道打开都需要关闭
+        producer.close();
+    }
+
+    private Producer<String, String> initProducer(){
+        // 配置
+        Properties properties = new Properties() ;
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, CommonConstants.BOOTSTRAP_SERVER_ADDRESS) ;
+        properties.setProperty(ProducerConfig.ACKS_CONFIG, "all") ;
+        properties.setProperty(ProducerConfig.RETRIES_CONFIG, "0") ;
+        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, "16384") ;
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "1") ;
+        properties.setProperty(ProducerConfig.BUFFER_MEMORY_CONFIG, "33554432") ;
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer") ;
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer") ;
+        // Producer 的主对象
+        Producer<String, String> producer = new KafkaProducer<>(properties) ;
+        return producer ;
+    }
+
+}
