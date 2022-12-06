b217035f167abf915ce12136edcc8aad6e3c2bc4
易成杰 <626659321@qq.com>
2022-12-06 11:29:21 +0800

    save

--- hello-kafka/src/test/java/com/yicj/study/kafka/admin/SSLConsumerSampleTest.java
+++ hello-kafka/src/test/java/com/yicj/study/kafka/admin/SSLConsumerSampleTest.java
@@ -1,6 +1,45 @@
 package com.yicj.study.kafka.admin;
 
+import com.yicj.study.kafka.constants.CommonConstants;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.junit.Test;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
 public class SSLConsumerSampleTest {
 
+    private final static String TOPIC_NAME = "hello-topic" ;
 
+    @Test
+    public void autoCommitConsumerTopic() throws InterruptedException {
+        Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, CommonConstants.SSL_BOOTSTRAP_SERVER_ADDRESS);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "test1");
+        props.put("enable.auto.commit", "true");
+        props.put("auto.commit.interval.ms", "1000");
+        // 这个配置会将订阅者的offset置为0，这样会接受队列中所有的消息
+        // props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest") ;
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        // ssl config
+        props.put("security.protocol","SSL");
+        props.put("ssl.endpoint.identification.algorithm","");
+        props.put("ssl.truststore.location","client.truststore.jks");
+        props.put("ssl.truststore.password","hello123");
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Arrays.asList(TOPIC_NAME));
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(10000));
+            for (ConsumerRecord<String, String> record : records){
+                String format = String.format("=========> partition: %s,  offset = %s, key = %s, value = %s",
+                        record.partition(), record.offset(), record.key(), record.value());
+                System.err.println(format);
+            }
+        }
+    }
 }
--- hello-kafka/src/test/java/com/yicj/study/kafka/admin/SSLProducerSampleTest.java
+++ hello-kafka/src/test/java/com/yicj/study/kafka/admin/SSLProducerSampleTest.java
@@ -1,12 +1,9 @@
 package com.yicj.study.kafka.admin;
 
 import com.yicj.study.kafka.constants.CommonConstants;
-import com.yicj.study.kafka.utils.CommonUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.producer.*;
 import org.junit.Test;
-
-import java.io.File;
 import java.util.Properties;
 import java.util.concurrent.Future;
 import java.util.stream.IntStream;
@@ -20,22 +17,21 @@ public class SSLProducerSampleTest {
     public void producer(){
         // 配置
         Properties properties = new Properties();
-        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"192.168.99.51:8989");
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, CommonConstants.SSL_BOOTSTRAP_SERVER_ADDRESS);
         properties.put(ProducerConfig.ACKS_CONFIG,"all");
         properties.put(ProducerConfig.RETRIES_CONFIG,"0");
         properties.put(ProducerConfig.BATCH_SIZE_CONFIG,"16384");
         properties.put(ProducerConfig.LINGER_MS_CONFIG,"1");
         properties.put(ProducerConfig.BUFFER_MEMORY_CONFIG,"33554432");
-
+        // ssl config
         properties.put("security.protocol","SSL");
         properties.put("ssl.endpoint.identification.algorithm","");
+        properties.put("ssl.truststore.location","client.truststore.jks");
+        properties.put("ssl.truststore.password","hello123");
         //File file = CommonUtils.copyClassPathTempFile(
         //        "client.truststore.jks", "client.truststore.jks");
         //properties.put("ssl.truststore.location",file.getPath());
         //log.info("===========> path : {}", file.getPath());
-        properties.put("ssl.truststore.location","client.truststore.jks");
-        properties.put("ssl.truststore.password","hello123");
-
         properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,"org.apache.kafka.common.serialization.StringSerializer");
         properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,"org.apache.kafka.common.serialization.StringSerializer");
         // Producer 的主对象
