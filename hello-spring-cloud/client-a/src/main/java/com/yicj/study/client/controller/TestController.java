c434714a29e54634089387c4104bb9a17208f607
易成杰 <626659321@qq.com>
2022-12-10 16:04:46 +0800

    save

--- hello-spring-cloud/client-a/src/main/java/com/yicj/study/client/controller/TestController.java
+++ hello-spring-cloud/client-a/src/main/java/com/yicj/study/client/controller/TestController.java
@@ -1,11 +1,21 @@
 package com.yicj.study.client.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api("测试集成Swagger")
 @RestController
 public class TestController {
 
+    @ApiOperation(value = "计算+", notes = "加法")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "a",  value = "数字a", required = true, dataType = "Long"),
+            @ApiImplicitParam(name = "b",  value = "数字b", required = true, dataType = "Long")
+    })
     @GetMapping("/add")
     public Integer add(Integer a, Integer b){
         return  a + b ;
