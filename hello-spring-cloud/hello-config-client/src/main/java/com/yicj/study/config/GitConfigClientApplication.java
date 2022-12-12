2061cf8b2d3ff0f78999d00b3763629cb0aa5dbd
易成杰 <626659321@qq.com>
2022-12-12 11:52:38 +0800

    save

---
+++ hello-spring-cloud/hello-config-client/src/main/java/com/yicj/study/config/GitConfigClientApplication.java
@@ -0,0 +1,13 @@
+package com.yicj.study.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitConfigClientApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(GitConfigClientApplication.class, args) ;
+    }
+}
