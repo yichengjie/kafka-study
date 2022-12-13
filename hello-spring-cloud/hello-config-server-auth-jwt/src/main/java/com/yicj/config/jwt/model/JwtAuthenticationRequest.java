dda14d49ebc9088ceb2cb2b0b1f3bcdcf9e2c552
易成杰 <626659321@qq.com>
2022-12-13 21:20:59 +0800

    save

---
+++ hello-spring-cloud/hello-config-server-auth-jwt/src/main/java/com/yicj/config/jwt/model/JwtAuthenticationRequest.java
@@ -0,0 +1,17 @@
+package com.yicj.config.jwt.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class JwtAuthenticationRequest implements Serializable {
+
+    private String username ;
+
+    private String password ;
+}
