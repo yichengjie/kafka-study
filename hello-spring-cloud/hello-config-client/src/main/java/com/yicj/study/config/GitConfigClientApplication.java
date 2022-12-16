8717b44d52fd338bc2fe7febcf837f6db2f11bc8
易成杰 <626659321@qq.com>
2022-12-16 19:52:40 +0800

    init

--- hello-spring-cloud/hello-config-client/src/main/java/com/yicj/study/config/GitConfigClientApplication.java
+++ hello-spring-cloud/hello-config-client/src/main/java/com/yicj/study/config/GitConfigClientApplication.java
@@ -1,13 +1,17 @@
 package com.yicj.study.config;
 
+import com.yicj.study.config.initializer.HelloApplicationContextInitializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
-public class GitConfigClientApplication {
+public class GitConfigClientApplication  {
 
     public static void main(String[] args) {
-
-        SpringApplication.run(GitConfigClientApplication.class, args) ;
+        SpringApplication springApplication = new SpringApplication(GitConfigClientApplication.class);
+        //springApplication.addInitializers(new HelloApplicationContextInitializer());
+        springApplication.run(args) ;
     }
 }
