cadd419381520ba0edb0abe0f201cdf0da68e808
易成杰 <626659321@qq.com>
2022-12-17 15:55:54 +0800

    save

--- hello-spring-cloud/hello-nacos-client/src/main/java/com/yicj/study/nacos/controller/ConfigController.java
+++ hello-spring-cloud/hello-nacos-client/src/main/java/com/yicj/study/nacos/controller/ConfigController.java
@@ -2,6 +2,7 @@ package com.yicj.study.nacos.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,8 @@ public class ConfigController {
 
     @Value("${useLocalCache:false}")
     private boolean useLocalCache;
+
+    private ConfigurableEnvironment environment ;
 
     @RequestMapping("/get")
     public boolean get() {
