42a9de8d566423a6f350d247d8fd2a088b9c16e5
易成杰 <626659321@qq.com>
2022-12-31 20:57:04 +0800

    save

--- hello-mvc/src/main/java/com/yicj/study/mvc/HelloMvcApplication.java
+++ hello-mvc/src/main/java/com/yicj/study/mvc/HelloMvcApplication.java
@@ -2,7 +2,9 @@ package com.yicj.study.mvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 
+@ConditionalOnWebApplication
 @SpringBootApplication
 public class HelloMvcApplication {
 
