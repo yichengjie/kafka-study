5ea615ca94e060962f7521ec16c49a1b10274a97
易成杰 <626659321@qq.com>
2023-01-19 20:34:16 +0800

    save

--- hello-mvc/src/test/java/com/yicj/study/web/BaseJunitTest.java
+++ hello-mvc/src/test/java/com/yicj/study/web/BaseJunitTest.java
@@ -3,10 +3,12 @@ package com.yicj.study.web;
 import com.yicj.study.mvc.HelloMvcApplication;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = HelloMvcApplication.class)
+@TestPropertySource(properties = {})
+@SpringBootTest(classes = HelloMvcApplication.class, properties = {})
 public abstract class BaseJunitTest {
 
 }
