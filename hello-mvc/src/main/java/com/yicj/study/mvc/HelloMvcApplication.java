f803c9eb688173ee75aa29d17938ea7d14398071
易成杰 <626659321@qq.com>
2023-01-31 22:15:40 +0800

    save

--- hello-mvc/src/main/java/com/yicj/study/mvc/HelloMvcApplication.java
+++ hello-mvc/src/main/java/com/yicj/study/mvc/HelloMvcApplication.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
