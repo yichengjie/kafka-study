0508939d8157f3402c73b04de7634e40322a3157
易成杰 <626659321@qq.com>
2023-02-04 18:22:56 +0800

    save

---
+++ hello-mybatis/src/main/java/com/yicj/jdbc/MybatisApplication.java
@@ -0,0 +1,16 @@
+package com.yicj.jdbc;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@Slf4j
+@SpringBootApplication
+public class MybatisApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(MybatisApplication.class, args) ;
+    }
+}
