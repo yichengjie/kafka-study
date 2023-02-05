f925a9730e6911b6a9e1d1238f5adb4b0dcc3bba
易成杰 <626659321@qq.com>
2023-02-05 20:19:26 +0800

    save

--- hello-mybatis/src/main/java/com/yicj/jdbc/MybatisApplication.java
+++ hello-mybatis/src/main/java/com/yicj/jdbc/MybatisApplication.java
@@ -1,16 +1,29 @@
 package com.yicj.jdbc;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
 @Slf4j
+@EnableTransactionManagement
 @SpringBootApplication
 public class MybatisApplication {
 
     public static void main(String[] args) {
 
         SpringApplication.run(MybatisApplication.class, args) ;
+
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory() ;
+        Resource resource = new ClassPathResource("bean.xml") ;
+        XmlBeanFactory xmlBeanFactory = new XmlBeanFactory(resource) ;
+
+        new ClassPathXmlApplicationContext("bean.xml") ;
     }
 }
