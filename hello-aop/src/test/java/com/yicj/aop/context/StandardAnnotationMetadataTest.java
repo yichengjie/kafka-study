9540d1dbd032669269e55be7ac0340d267767e25
易成杰 <626659321@qq.com>
2023-02-26 12:03:07 +0800

    补充mvc缓存测试

---
+++ hello-aop/src/test/java/com/yicj/aop/context/StandardAnnotationMetadataTest.java
@@ -0,0 +1,27 @@
+package com.yicj.aop.context;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.core.type.StandardAnnotationMetadata;
+
+import java.util.Map;
+
+
+@Slf4j
+public class StandardAnnotationMetadataTest {
+
+    @EnableAutoConfiguration
+    static class Anno{
+
+    }
+    @Test
+    public void from(){
+        EnableAutoConfiguration annotation = Anno.class.getAnnotation(EnableAutoConfiguration.class);
+        StandardAnnotationMetadata metadata =  new StandardAnnotationMetadata(Anno.class, true);;
+        Map<String, Object> attributes = metadata.getAnnotationAttributes(EnableAutoConfiguration.class.getName());
+        attributes.forEach((key, value) -> {
+            log.info("=====> key : {}, value: {}", key, value);
+        });
+    }
+}
