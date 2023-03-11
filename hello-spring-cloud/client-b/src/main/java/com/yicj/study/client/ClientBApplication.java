75db3ac4861a4af818112f52988a9bdf2ea56ca5
yichengjie <626659321@qq.com>
2023-03-11 11:06:37 +0800

    save

--- hello-spring-cloud/client-b/src/main/java/com/yicj/study/client/ClientBApplication.java
+++ hello-spring-cloud/client-b/src/main/java/com/yicj/study/client/ClientBApplication.java
@@ -1,13 +1,21 @@
 package com.yicj.study.client;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
 
+@Slf4j
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ClientBApplication {
@@ -16,7 +24,6 @@ public class ClientBApplication {
         SpringApplication.run(ClientBApplication.class, args) ;
     }
 
-    
     @Bean
     @LoadBalanced
     public RestTemplate restTemplate(){
---
+++ hello-spring-cloud/client-b/src/test/java/com/yicj/study/client/utils/DiscoveryClientTest.java
@@ -0,0 +1,25 @@
+package com.yicj.study.client.utils;
+
+import com.yicj.study.client.BaseJunitTest;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+
+import java.util.List;
+
+@Slf4j
+public class DiscoveryClientTest extends BaseJunitTest {
+
+    @Autowired
+    private DiscoveryClient discoveryClient ;
+
+    @Test
+    public void getInstances(){
+        String serviceId = "client-a" ;
+        List<ServiceInstance> list = discoveryClient.getInstances(serviceId);
+        log.info("instance size : {}", list.size());
+        list.forEach(instance -> log.info("instance : host : {}, port : {}", instance.getHost(), instance.getPort()));
+    }
+}
