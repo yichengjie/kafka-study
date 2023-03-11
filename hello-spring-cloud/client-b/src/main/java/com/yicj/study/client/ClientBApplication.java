971069aea7fb9fda95c300258889d4859cfd313d
yichengjie <626659321@qq.com>
2023-03-11 10:05:34 +0800

    save

--- hello-spring-cloud/client-b/src/main/java/com/yicj/study/client/ClientBApplication.java
+++ hello-spring-cloud/client-b/src/main/java/com/yicj/study/client/ClientBApplication.java
@@ -3,6 +3,9 @@ package com.yicj.study.client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 
 @EnableDiscoveryClient
@@ -10,7 +13,13 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class ClientBApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(ClientBApplication.class, args) ;
+    }
+
+    
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate() ;
     }
 }
