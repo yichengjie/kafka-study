53985bd38127f20184d4beebcd6f1dc984f505e0
yichengjie <626659321@qq.com>
2023-03-11 21:51:04 +0800

    feign提成单独项目

--- hello-spring-cloud/hello-openfeign/src/main/java/com/yicj/study/feign/remote/feign/HelloFeignClient.java
+++ hello-spring-cloud/hello-openfeign/src/main/java/com/yicj/study/feign/remote/feign/HelloFeignClient.java
@@ -3,7 +3,7 @@ package com.yicj.study.feign.remote.feign;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(value = "client-b", contextId = "helloFeignClient")
+@FeignClient(name = "client-b", contextId = "helloFeignClient")
 public interface HelloFeignClient {
 
     @GetMapping("/test")
