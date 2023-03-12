d6200cef61ea7f0c4169ed8667d54d3498f4ee33
yichengjie <626659321@qq.com>
2023-03-12 10:47:58 +0800

    删除迁出模块

--- hello-auth/hello-auth-gateway/src/main/java/com/yicj/study/gateway/filter/PermissionException.java
+++
@@ -1,8 +0,0 @@
-package com.yicj.study.gateway.filter;
-
-public class PermissionException extends RuntimeException {
-	private static final long serialVersionUID = 1L;
-	public PermissionException(String msg) {
-        super(msg);
-    }
-}
--- hello-spring-cloud/hello-sentinel-client/src/main/java/com/yicj/study/sentinel/service/HelloService.java
+++
@@ -1,6 +0,0 @@
-package com.yicj.study.sentinel.service;
-
-public interface HelloService {
-
-    void doSomething() ;
-}
